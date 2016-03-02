461325fbdd51ee448807b366f22257f21325d488
Jamie Patel <jamie@notespublication.com>
2016-03-02 16:13:29 +0000

    Make more demoable

--- shared_worker.js
+++ shared_worker.js
@@ -14,11 +14,10 @@ var channel = pusher.subscribe('test_channel');
 // Bind to my event
 channel.bind('my_event', function(data) {
 
-  // For each client, relay the message on to them
-  for (var i = 0; i < clients.length; i++) {
-    var port = clients[i];
-    port.postMessage(data);
-  }  
+  // Relay the message on to each client
+  clients.forEach(function(client){
+    client.postMessage(data);
+  });
 });
 
 self.addEventListener("connect", function(evt){
@@ -26,16 +25,6 @@ self.addEventListener("connect", function(evt){
   // Add the port to the list of connected clients
   var port = evt.ports[0];
   clients.push(port);
-
-  port.addEventListener('message', function(evt) {
-    var message = evt.data;
-    if (message === "disconnect") {
-      // Whenever we get the `disconnect` event from the client, disconnect from Pusher
-      // and shut down the worker
-      pusher.disconnect();
-      self.port.close();
-    }
-  });
 
   // Start the worker.
   port.start();
